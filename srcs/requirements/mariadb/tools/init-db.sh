c198278c82545b17ad9976a7b2412b206c9c93d2
igoryan <igoryan@debian.myguest.virtualbox.org>
2026-02-04 09:34:45 +0100

    debuged WP script

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -2,15 +2,10 @@
 set -e
 
 Reset='\033[0m'       # Text Reset
-
-Black='\033[0;30m'        # Black
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
 Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
 
 # Creat directories for MySQL service
 mkdir -p /var/log/mysql && chmod -R 777 /var/log/mysql
@@ -35,22 +30,13 @@ until mysqladmin --socket=/run/mysqld/mysqld.sock ping > /dev/null 2>&1; do
 done
 echo -e ${Green}MariaDB is ready.${Reset}
 
-MYSQL_ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD")
-MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD")
-export MYSQL_ROOT_PASSWORD
-export MYSQL_PASSWORD
-
-echo -e ${Cyan}MYSQL_ROOT_PASSWORD: ${Yellow} ${MYSQL_ROOT_PASSWORD} ${Reset}
-echo -e ${Cyan}MYSQL_PASSWORD: ${Yellow} ${MYSQL_PASSWORD} ${Reset}
-
-
 # Run setup SQL: create database and users
 echo -e ${Yellow}Running setup SQL...${Reset}
 mysql -u root << EOF
 FLUSH PRIVILEGES;
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$(cat "$MYSQL_ROOT_PASSWORD")');
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '$(cat "$MYSQL_PASSWORD")';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
--- srcs/requirements/wordpress/tools/wp-install.sh
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-Reset='\033[0m'       # Text Reset
-
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
 set -e         
+
+Reset='\033[0m'     
+Red='\033[0;31m'         
+Green='\033[0;32m'       
+Yellow='\033[0;33m'       
+Blue='\033[0;34m'       
+Purple='\033[0;35m'      
 
 echo -e ${Yellow}Starting WordPress setup...${Reset}
 
@@ -24,7 +20,7 @@ fi
 if [ ! -f /var/www/html/wp-config.php ] && [ ! -f /var/www/html/index.php ]; then
     echo -e     ${Red}The WordPress not existed.${Reset}
     echo -e     ${Blue}Downloading...${Reset}
-    wget        https://wordpress.org/latest.tar.gz -O /tmp/wp.tar.gz
+    wget        https://wordpress.org/latest.tar.gz >/dev/null 2>&1 -O /tmp/wp.tar.gz
     tar  -xzf   /tmp/wp.tar.gz -C /tmp/
     mv          /tmp/wordpress/* /var/www/html/
     rm   -rf    /tmp/wp.tar.gz /tmp/wordpress
@@ -33,11 +29,13 @@ fi
 
 export i=1 max=30
 
-until mysql --host      ${WORDPRESS_DB_HOST}            \
-            --port      ${WORDPRESS_DB_PORT}            \
-            --user      ${WORDPRESS_DB_USER}            \
-            --password  $(cat $WORDPRESS_DB_PASSWORD)   \
-            -e          "SELECT 1" >/dev/null 2>&1;     do
+echo -e ${Blue}Going etempt to establish connection...${Reset}
+
+until mysql -h ${WORDPRESS_DB_HOST}          \
+            -p ${WORDPRESS_DB_PORT}          \
+            -u ${WORDPRESS_DB_USER}          \
+            -p$(cat $WORDPRESS_DB_PASSWORD)  \
+            -e "SELECT 1" >/dev/null 2>&1;  do
     sleep 1
     export i=$(($i + 1))
     echo    -e ${Purple}Etempt to reestablish connection...${Reset}
@@ -46,6 +44,8 @@ until mysql --host      ${WORDPRESS_DB_HOST}            \
         exit 1
     fi
 done
+
+echo -e ${Green}Connection to the MariaDB seccessfuly established.${Reset}
 
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
